0021ed3834585e6c21507b614f2ca20731c9edc6
Shruti Karthikeyan <160132808+Shrulex@users.noreply.github.com>
2024-10-05 09:47:04 +0530

    Bottle_ship.java

--- Bottle_ship.java
+++ Bottle_ship.java
@@ -4,23 +4,22 @@ public class BottleShipping {
     public static void shipBottles(int numBottles) {
         int xl = numBottles / 48;
         numBottles = numBottles % 48;
-
+        
         int large = numBottles / 24;
         numBottles = numBottles % 24;
-
+        
         int medium = numBottles / 12;
         numBottles = numBottles % 12;
-
+        
         int small = numBottles / 6;
-
+        if (numBottles % 6 != 0) {
+            small++; // account for remaining bottles
+        }
+        
         System.out.print(xl + " xl, " + large + " large, " + medium + " medium, " + small + " small");
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of bottles: ");
-        int inputBottles = scanner.nextInt();
-        shipBottles(inputBottles);
+        shipBottles(140);
     }
 }
-
